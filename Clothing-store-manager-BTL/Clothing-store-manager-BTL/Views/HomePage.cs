0b4dc07a75db01f40b70c47c64f106b64afa5be5
Trần Văn Tùng <74578965+tranvantung2002@users.noreply.github.com>
2022-09-18 11:05:29 +0700

    Cơ bản của trang chủ

---
+++ Clothing-store-manager-BTL/Clothing-store-manager-BTL/Views/HomePage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clothing_store_manager_BTL.Views
+{
+    public partial class HomePage : Form
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            Ttime.Start();
+        }
+
+        private void Ttime_Tick(object sender, EventArgs e)
+        {
+            LBtime.Text = DateTime.Now.ToString();
+        }
+    }
+}
